f8c989d3d89be1e24805a970d12451e3b7639a6b
Hasnaint037 <hasnaint037@gmail.com>
2025-10-21 12:41:01 +0500

    add schema method for password hashing and comparing password

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface User extends Document {
   name: string;
@@ -6,10 +7,11 @@ export interface User extends Document {
   password: string;
   profilePicture?: string;
   bio?: string;
-  followers: mongoose.Types.ObjectId[];
-  following: mongoose.Types.ObjectId[];
+  followers?: mongoose.Types.ObjectId[];
+  following?: mongoose.Types.ObjectId[];
   resetPasswordToken?: string;
   resetPasswordExpires?: Date;
+  comparePassword: (candidatePassword: string) => Promise<boolean>;
 }
 
 const userSchema: Schema<User> = new Schema(
@@ -24,9 +26,23 @@ const userSchema: Schema<User> = new Schema(
     resetPasswordToken: { type: String },
     resetPasswordExpires: { type: Date },
   },
-  { timestamps: true } // adds createdAt and updatedAt automatically
+  { timestamps: true }
 );
 
 const User = mongoose.model<User>("User", userSchema);
+
+userSchema.pre<User>("save", async function (next) {
+  // only if password is changed or new
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 export default User;
